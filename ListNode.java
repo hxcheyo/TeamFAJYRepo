d134e245f1470a32a50a351a1975ff8f3bab10e6
hxcheyo <ane6td@virginia.edu>
2021-09-29 17:52:30 -0500

    Update ListNode.java
    I would be grateful if Yena could review my pull request and merge this branch into main!

--- ListNode.java
+++ ListNode.java
@@ -1,3 +1,10 @@
+/* Java made me include this class
+ * It is not part of the instructions
+ * I am now making these changes to give Yena a chance to practice
+ * merging a pull request. This is because I did work on the main branch.
+ * I should feel bad about that.
+ */
+
 public class ListNode {
      int val;
      ListNode next;
